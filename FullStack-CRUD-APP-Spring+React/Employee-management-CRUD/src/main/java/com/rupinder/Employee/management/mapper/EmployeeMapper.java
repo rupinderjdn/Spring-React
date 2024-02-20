ce8cd28a0b6b13c51dc8fc4bbdbfb78522d74d68
Rupinder <rupinder.singh@finmechanics.com>
2024-02-20 10:19:00 +0530

    @part6+7 @adding Employee mapper and Employee Dto

---
+++ FullStack-CRUD-APP-Spring+React/Employee-management-CRUD/src/main/java/com/rupinder/Employee/management/mapper/EmployeeMapper.java
@@ -0,0 +1,14 @@
+package com.rupinder.Employee.management.mapper;
+
+import com.rupinder.Employee.management.dto.EmployeeDto;
+import com.rupinder.Employee.management.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(employee.getId(), employee.getFirstName(), employee.getFirstName(), employee.getEmail());
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(employeeDto.getId(), employeeDto.getFirstName(), employeeDto.getFirstName(), employeeDto.getEmail());
+    }
+}
